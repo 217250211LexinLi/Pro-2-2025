74ca4939db36f9b5de03859e62b72872dd54cb42
217250211LexinLi <2184807894@qq.com>
2026-03-11 09:42:03 +0800

    ERyder

--- Main.java
+++ Main.java
@@ -1,11 +1,16 @@
 public class Main {
-    public static void main(String[] args) {
-ERyder bikeDefault = new ERyder();
-System.out.println("Bike created with Default Constructor");
-bikeDefault.printBikeDetails();
-ERyder bikeFull = new ERyder(1001, 75, true, 123.5);
-System.out.println("Bike created with Parameterized Constructor");
-bikeFull.ride();
-bikeFull.printBikeDetails();
-    }
+public static void main(String[] args) {
+ ERyder bike1 = new ERyder();
+ bike1.printBikeDetails();
+ ERyder bike2 = new ERyder();
+ bike2.bikeID=1;
+ bike2.batteryLevel=70;
+ bike2.isAvailable=true;
+ bike2.kmDriven=100.0;
+ bike2.ride();
+ bike2.printBikeDetails();
+bike2.printRideDetails(15);
+  double fare = bike2.calculateFare();
+ System.out.println("Ride fare: $" + fare);
+ }
 }
