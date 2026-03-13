d3bbd89782736a01beede1e9e2b8056b4e111dac
217250211LexinLi <2184807894@qq.com>
2026-03-13 21:41:15 +0800

    Exercise5

--- ERyder.java
+++ ERyder.java
@@ -2,8 +2,8 @@ public class ERyder {
 public static final String COMPANY_NAME = "ERyder";
 public static final double BASE_FARE = 1.0;
 public static final double PER_MINUTE_FARE = 0.5;
-private  final String LINKED_ACCOUNT;
-private  final String LINKED_PHONE_NUMBER ;
+private final String LINKED_ACCOUNT;
+private final String LINKED_PHONE_NUMBER ;
  int bikeID;
 int batteryLevel;
 int rideTime;
@@ -18,8 +18,8 @@ private double totalFare;
  }
 
  public ERyder(int id, int bat, boolean ava, double km,  String linkedAccount, String linkedPhoneNumber) {
-  bikeID = id;
-  setBatteryLevel(bat); 
+ bikeID = id;
+setBatteryLevel(bat); 
  isAvailable = ava;
  kmDriven = km;
 this.LINKED_ACCOUNT = linkedAccount;
@@ -33,16 +33,16 @@ this.LINKED_ACCOUNT = linkedAccount;
  }
  }
  public void printBikeDetails() {
-    System.out.println("Bike ID: " + bikeID);
-    System.out.println("Battery Level: " + batteryLevel + "%");
-    System.out.println("Is Available: " + isAvailable);
-    System.out.println("KM Driven: " + kmDriven + "km\n");
-    }
-    public void printRideDetails(int rideTime) {
+ System.out.println("Bike ID: " + bikeID);
+ System.out.println("Battery Level: " + batteryLevel + "%");
+ System.out.println("Is Available: " + isAvailable);
+ System.out.println("KM Driven: " + kmDriven + "km\n");
+ }
+public void printRideDetails(int rideTime) {
  this.rideTime=rideTime;
-    System.out.println("Ride duration: " + rideTime + " minutes");
-    System.out.println("Current bike ID: " + bikeID);
-    System.out.println("User account: " + LINKED_ACCOUNT);
+  System.out.println("Ride duration: " + rideTime + " minutes");
+ System.out.println("Current bike ID: " + bikeID);
+ System.out.println("User account: " + LINKED_ACCOUNT);
 }
  public void setBatteryLevel(int bat) {
 if (bat <0)
@@ -51,9 +51,9 @@ if (bat <0)
  batteryLevel=100;
  else
  batteryLevel=bat;
-    }
-    public double calculateFare() {
-    double fare = 0;
+ }
+ public double calculateFare() {
+ double fare = 0;
  if (rideTime <= 30) {
  fare = 2;
  } else {
